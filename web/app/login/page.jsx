36e77d0c4f3fc080bf03d40e95bad6da2e66e28a
kostiago <tiago_pb-@hotmail.com>
2024-04-18 11:58:47 -0300

    feat: Login page creation

--- web/app/login/page.jsx
+++ web/app/login/page.jsx
@@ -1,34 +1,82 @@
+"use client";
+import { useState } from "react";
 import styles from "./login.module.css";
+import {
+  MdOutlineEmail,
+  MdOutlineLock,
+  MdOutlineRemoveRedEye,
+  MdVisibility,
+  MdVisibilityOff,
+} from "react-icons/md";
+import Image from "next/image";
 
 const LoginPage = () => {
-  return (
-    <div className={styles.container}>
-      <div className={styles.leftLogin}>
-        <h1>IMAGEM AQUI</h1>
-      </div>
-      <div className={styles.rightLogin}>
-        <div className={styles.cardLogin}>
-          <h1>Entrar</h1>
-          <div className={styles.textfield}>
-            <label for="emailUser">Email</label>
-            <input
-              type="email"
-              name="emailUser"
-              placeholder="Digite seu email de usuario"
-            />
-          </div>
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-          <div className={styles.textfield}>
-            <label for="password">Senha</label>
-            <input
-              type="password"
-              name="password"
-              placeholder="Digite sua senha"
-            />
+  const togglePasswordVisibility = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
+  return (
+    <section>
+      <header className={styles.logo}>
+        <Image src="/assets/logo.svg" width="250" height="250" alt="logo" />
+      </header>
+      <div className={styles.container}>
+        <div className={styles.leftLogin}>
+          <Image src="/assets/man.svg" width="600" height="600" alt="logo" />
+        </div>
+        <div className={styles.rightLogin}>
+          <div className={styles.cardLogin}>
+            <div className={styles.title}>
+              <h1>Login</h1>
+            </div>
+
+            <div className={styles.textfield}>
+              <div className="">
+                <MdOutlineEmail />
+                <label for="emailUser">Email</label>
+              </div>
+
+              <input
+                type="email"
+                name="emailUser"
+                placeholder="Digite seu email de usuario"
+              />
+            </div>
+
+            <div className={styles.textfield}>
+              <label for="password">
+                <MdOutlineLock />
+                Senha
+              </label>
+
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Digite sua senha"
+              />
+
+              <button
+                className={styles.showPassword}
+                type="button"
+                onClick={togglePasswordVisibility}
+              >
+                {showPassword ? (
+                  <MdVisibilityOff size={20} />
+                ) : (
+                  <MdVisibility size={20} />
+                )}
+              </button>
+            </div>
+            <button className={styles.btnLogin}>Entrar</button>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
