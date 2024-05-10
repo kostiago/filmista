4962d458298374b32120cd310d76c8dc17de06de
kostiago <tiago_pb-@hotmail.com>
2024-05-10 17:00:45 -0300

    feat: continuação da sidebar

--- client-web/src/components/back-office/Dashboard/Sidebar/Sidebar.tsx
+++ client-web/src/components/back-office/Dashboard/Sidebar/Sidebar.tsx
@@ -4,7 +4,8 @@ import styles from "./sidebar.module.scss";
 import Logo from "/public/images/placeholder.svg";
 import Link from "next/link";
 import { BsArrowRightSquareFill, BsArrowLeftSquareFill } from "react-icons/bs";
-import { MdOutlineHome } from "react-icons/md";
+import { AiOutlineVideoCameraAdd } from "react-icons/ai";
+import { TbMessages, TbCreditCard, TbLayoutDashboard } from "react-icons/tb";
 import { useState } from "react";
 
 const Sidebar = () => {
@@ -35,14 +36,91 @@ const Sidebar = () => {
             <li className={styles.menuItem}>
               <Link href={"/dashboard"}>
                 <div className={styles.menuIcon}>
-                  <MdOutlineHome />
+                  <TbLayoutDashboard />
                 </div>
                 Dashboard
+              </Link>
+            </li>
+
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <AiOutlineVideoCameraAdd />
+                </div>
+                Novo Conteudo
+              </Link>
+
+              <ul className={styles.subMenu}>
+                <li>
+                  <Link href={"#"}>Filmes</Link>
+                </li>
+
+                <li>
+                  <Link href={"#"}>Series</Link>
+                </li>
+
+                <li>
+                  <Link href={"#"}>Documentarios</Link>
+                </li>
+              </ul>
+            </li>
+
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <TbMessages />
+                </div>
+                Relátorios
+              </Link>
+            </li>
+
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <TbCreditCard />
+                </div>
+                Planos
               </Link>
             </li>
           </ul>
         </div>
       </div>
+      <div className={styles.menu}>
+        <p className={styles.title}>Conta</p>
+
+        <div className={styles.mainMenu}>
+          <ul>
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <TbLayoutDashboard />
+                </div>
+                Notificações
+              </Link>
+            </li>
+
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <AiOutlineVideoCameraAdd />
+                </div>
+                Configurações
+              </Link>
+            </li>
+
+            <li className={styles.menuItem}>
+              <Link href={"/dashboard"}>
+                <div className={styles.menuIcon}>
+                  <TbMessages />
+                </div>
+                FAQ
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className={styles.sideMenuFooter}></div>
     </div>
   );
 };
