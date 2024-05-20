01d96f0c6999821a52e33ed72f307b621b793d85
kostiago <tiago_pb-@hotmail.com>
2024-05-20 20:41:39 -0300

    feat: cards

--- client-web/src/components/back-office/Dashboard/HomeDashboard/Overview/DashboardOverview.tsx
+++ client-web/src/components/back-office/Dashboard/HomeDashboard/Overview/DashboardOverview.tsx
@@ -2,12 +2,15 @@ import styles from "./dashboardOverview.module.scss";
 import { TfiVideoCamera } from "react-icons/tfi";
 import { TbMovie } from "react-icons/tb";
 import Card from "@/components/card/Card";
+import { BsPatchCheck } from "react-icons/bs";
 const DashboardOverview = () => {
   return (
     <section className={styles.container}>
       <div className={styles.content}>
         <h2>Conteudo adicionado na plataforma</h2>
         <div className={styles.cardContainer}>
+          <Card />
+          <Card />
           <Card />
         </div>
       </div>
@@ -16,8 +19,26 @@ const DashboardOverview = () => {
         <h2>Resumo da plataforma</h2>
 
         <div className={styles.card}>
-          <div className={styles.cardIcon}></div>
-          <div className={styles.cardContent}></div>
+          <div className={styles.cardIcon}>
+            <BsPatchCheck />
+          </div>
+          <div className={styles.cardContent}>
+            <h4>Disponivel na plataforma</h4>
+            <small>
+              TOTAL: <h4>50</h4>
+            </small>
+          </div>
+        </div>
+        <div className={styles.card}>
+          <div className={styles.cardIcon}>
+            <BsPatchCheck />
+          </div>
+          <div className={styles.cardContent}>
+            <h4>Disponivel na plataforma</h4>
+            <small>
+              TOTAL: <h4>50</h4>
+            </small>
+          </div>
         </div>
       </div>
     </section>
